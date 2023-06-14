1aa9da571aa7cfb0e9bf68b2ebff9bc6890d37e9
DineshRajK <dineshraj.kamd@gmail.com>
2023-06-14 17:30:04 +0530

    second commit

--- Models/Department.cs
+++ Models/Department.cs
@@ -6,7 +6,7 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int Members { get; set; }
-        public string? Owner { get; set; }
+        public string? Faculty { get; set; }
 
     }
 }
