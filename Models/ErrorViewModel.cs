081639840ba97493bc3e9fe64008839779ad6ce3
DineshRajK <dineshraj.kamd@gmail.com>
2023-06-14 17:21:04 +0530

    All commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevopsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace DevopsDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public String? Course { get; set; }
+        public int Age { get; set; }
+    }
+}
